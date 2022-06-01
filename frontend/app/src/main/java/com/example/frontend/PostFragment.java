7294fdb8eaf349a3dd444b3ac56e765954697164
ALin <jl-zhang18@mails.tsinghua.edu.cn>
2022-06-01 22:23:03 +0800

    stash: still a lot to do

--- frontend/app/src/main/java/com/example/frontend/PostFragment.java
+++ frontend/app/src/main/java/com/example/frontend/PostFragment.java
@@ -2,7 +2,10 @@ package com.example.frontend;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 import butterknife.BindView;
 
 import android.util.Log;
@@ -12,14 +15,22 @@ import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PostFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class PostFragment extends Fragment {
+    private  static final int TAB_NUM = 2;
+    @BindView(R.id.post_tab)
+    protected TabLayout mtabLayout;
+    @BindView(R.id.post_viewpager)
+    protected ViewPager2 mViewPager;
 
-
+    private List<Fragment> mFragments;
 
     public PostFragment() {
         // Required empty public constructor
@@ -32,6 +43,19 @@ public class PostFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mFragments = new ArrayList<>();
+        this.mViewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                // TODO : Bind all frags here
+                return null;
+            }
+
+            @Override
+            public int getItemCount()
+            {return TAB_NUM;}
+        });
     }
 
     @Override
